b20d6e76b839e681a9e3db1cb5013215b00f0730
Antoine Ross <amoropesa@up.edu.ph>
2023-04-20 22:09:25 +0800

    separated a python code to be run on external GPU.

---
+++ signlanguage.py
@@ -0,0 +1,131 @@
+# 1. Import Dependencies
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+import os
+import mediapipe as mp
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+
+print("Imported Libraries.")
+
+# 2. Capturing video and Drawing keypoints using MP Holistic
+#Keypoints
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+    
+def draw_styled_landmarks(image, results):
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                             ) 
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             )
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             )
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             )
+    
+print("Functions for mediapipe detection and styled landmarks defined.")
+
+# 3. Extracting Keypoints Values into a numpy array
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4) 
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh]) 
+  
+print("Keypoints extracted.")
+  
+# 4. Setting up variables for Data Collection
+DATA_PATH = os.path.join('MP_Data')                 # Path for exported data, numpy arrays
+actions = np.array(['hello', 'thanks', 'iloveyou']) # Actions that we try to detect
+no_sequences = 30                                   # Thirty videos worth of data
+sequence_length = 30                                # Videos are going to be 30 frames in length
+
+# 5. Preprocess Data and Create Labels and Features
+label_map = {label:num for num, label in enumerate(actions)}
+
+sequences, labels = [], []
+for action in actions:
+    for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+        
+X = np.array(sequences)
+
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+print("Data preprocessed and split into train and test sets.")
+
+# 6. Build and train the LSTM Model
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
+model.summary()
+model.save('action.h5')
+
+print("LSTM Model trained.")
+
+# 7. Evaluation using Confusion Matrix and Accuracy
+yhat = model.predict(X_test)
+ytrue = np.argmax(y_test, axis=1).tolist()
+yhat = np.argmax(yhat, axis=1).tolist()
+multilabel_confusion_matrix(ytrue, yhat)
+accuracy_score(ytrue, yhat)
+
+print("Multilabel Confusion Matrix: %s\nAccuracy Score: %s" % (multilabel_confusion_matrix(ytrue, yhat), accuracy_score(ytrue, yhat)))
+
+# 8. Test in Real Time
+
+
+
+
+
